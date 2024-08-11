990022213eaf503df43189525dcd35e742f11a48
peleda23 <kosmosas23@gmail.com>
2024-08-11 16:30:48 +0300

    Initial commit

---
+++ Day 5/Password Generator Project/task.py
@@ -0,0 +1,39 @@
+import random
+from random import shuffle
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+new_password = ""
+first_password = ""
+
+
+for i in range(0, nr_letters):
+    password += random.choice(letters)
+    first_password += letters[i]
+
+for i in range(0, nr_symbols):
+    password += random.choice(symbols)
+    first_password += symbols[i]
+
+for i in range(0, nr_numbers):
+    password += random.choice(numbers)
+    first_password += numbers[i]
+
+
+random.shuffle(password)
+
+for i in password:
+    new_password += i
+
+
+print(first_password)
+print(new_password)
+
